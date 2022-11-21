3544d279db85d6eaba945be64178517c8be567bf
Harry Slaughter <harry.slaughter@sekon.com>
2022-11-21 11:47:38 -0600

    [OCWT-3447] Also truncate tables we don't need to copy over or may contain bits of PII.

--- scripts/store_sanitize_pii.sql
+++ scripts/store_sanitize_pii.sql
@@ -4,6 +4,46 @@
 -- This script can be run from the project root using: drush sql:cli < scripts/store_sanitize_pii.sql
 --
 
+-- Empty tables we do not need to carry over at all
+TRUNCATE `cache_admin_menu`;
+TRUNCATE `cache_block`;
+TRUNCATE `cache_bootstrap`;
+TRUNCATE `cache_config`;
+TRUNCATE `cache_container`;
+TRUNCATE `cache_data`;
+TRUNCATE `cache_default`;
+TRUNCATE `cache_discovery`;
+TRUNCATE `cache_dynamic_page_cache`;
+TRUNCATE `cache_entity`;
+TRUNCATE `cache_explinklist`;
+TRUNCATE `cache_features`;
+TRUNCATE `cache_feeds_http`;
+TRUNCATE `cache_field`;
+TRUNCATE `cache_filter`;
+TRUNCATE `cache_form`;
+TRUNCATE `cache_htmlpurifier`;
+TRUNCATE `cache_image`;
+TRUNCATE `cache_libraries`;
+TRUNCATE `cache_menu`;
+TRUNCATE `cache_metatag`;
+TRUNCATE `cache_page`;
+TRUNCATE `cache_path`;
+TRUNCATE `cache_render`;
+TRUNCATE `cache_rest`;
+TRUNCATE `cache_rules`;
+TRUNCATE `cache_token`;
+TRUNCATE `cache_toolbar`;
+TRUNCATE `cache_update`;
+TRUNCATE `cache_variable`;
+TRUNCATE `cache_views`;
+TRUNCATE `cache_views_data`;
+TRUNCATE `ctools_css_cache`;
+TRUNCATE `ctools_object_cache`;
+TRUNCATE `views_data_export_object_cache`;
+TRUNCATE `semaphore`;
+TRUNCATE `sessions`;
+TRUNCATE `watchdog`;
+
 --
 -- Basic Drupal user tables
 --
