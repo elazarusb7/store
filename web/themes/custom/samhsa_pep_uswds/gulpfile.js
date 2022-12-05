432ce4e91cb8b870b1b3d11e4c25c13a88bf1dc3
Harry Slaughter <harry.slaughter@sekon.com>
2022-12-05 10:26:12 -0600

    [OCWT-3478] Include eslint packages; apply eslint fixes.

--- web/themes/custom/samhsa_pep_uswds/gulpfile.js
+++ web/themes/custom/samhsa_pep_uswds/gulpfile.js
@@ -2,7 +2,7 @@
 /* jshint esversion: 6, node: true */
 
 const gulp = require("gulp");
-const sass = require('gulp-sass')(require('sass'));
+const sass = require("gulp-sass")(require("sass"));
 // Inline source maps are embedded in the source file
 const sourcemaps = require("gulp-sourcemaps");
 // Parse CSS and add vendor prefixes to CSS rules. Example "-webkit-min-device-pixel-ratio"
@@ -15,27 +15,13 @@ const minify = require("gulp-clean-css");
 ----------------------------------------
 PATHS
 ----------------------------------------
-- All paths are relative to the
-  project root
-- Don't use a trailing `/` for path
-  names
+- All paths are relative to theme directory
+- Don't use a trailing `/` for path names
 ----------------------------------------
 */
 
 // Project Sass source directory
 const PROJECT_SASS_SRC = "./src/sass/styles.scss";
-
-// Images destination
-// const IMG_DEST = "./assets/img";
-
-// Fonts destination
-// const FONTS_DEST = "./assets/fonts";
-
-// Javascript destination
-// const JS_DEST = "./assets/js";
-
-// Compiled CSS destination
-const CSS_DEST = "./css";
 
 // Site CSS destination
 // Like the _site/assets/css directory in Jekyll, if necessary.
@@ -48,30 +34,29 @@ TASKS
 ----------------------------------------
 */
 
-gulp.task("build-sass", function (done) {
-  let plugins = [
+gulp.task("build-sass", () => {
+  const plugins = [
     autoprefixer({
-      cascade: false, grid: true
-    })
+      cascade: false,
+      grid: true,
+    }),
   ];
-  return (
-    gulp.src(`${PROJECT_SASS_SRC}`)
-      .pipe(sourcemaps.init({largeFile: true}))
-      .pipe(sass.sync().on('error', sass.logError))
-      .pipe(postcss(plugins))
-      .pipe(minify())
-      .pipe(sourcemaps.write())
-      .pipe(gulp.dest(`${SITE_CSS_DEST}`))
-    );
+  return gulp
+    .src(`${PROJECT_SASS_SRC}`)
+    .pipe(sourcemaps.init({ largeFile: true }))
+    .pipe(sass.sync().on("error", sass.logError))
+    .pipe(postcss(plugins, undefined))
+    .pipe(minify())
+    .pipe(sourcemaps.write(undefined, undefined))
+    .pipe(gulp.dest(`${SITE_CSS_DEST}`));
 });
 
 gulp.task("build", gulp.series("build-sass"));
 
-gulp.task("watch-sass", function () {
+gulp.task("watch-sass", () => {
   gulp.watch("./src/sass/**/*.scss", gulp.series("build-sass"));
 });
 
 gulp.task("build", gulp.series("build-sass"));
 gulp.task("watch", gulp.series("watch-sass"));
 gulp.task("default", gulp.series("build"));
-
